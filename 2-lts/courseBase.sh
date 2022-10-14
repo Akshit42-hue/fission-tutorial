91a7eeaaf62e1c93e0dcd56ae14219680abd8e5c
Akshit42-hue <patelakshit2025@gmail.com>
2022-10-14 21:38:10 +0530

    added_fission_image
    Signed-off-by: Akshit42-hue <patelakshit2025@gmail.com>

--- 2-lts/courseBase.sh
+++ 2-lts/courseBase.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-export FISSION_NAMESPACE="fission"
-kubectl create namespace $FISSION_NAMESPACE
-kubectl create -k "github.com/fission/fission/crds/v1?ref=v1.17.0"
-helm repo add fission-charts https://fission.github.io/fission-charts/
-helm repo update
-helm install --version v1.17.0 --namespace $FISSION_NAMESPACE fission \
-  --set serviceType=NodePort,routerServiceType=NodePort \
-  fission-charts/fission-all
-  
+
+docker pull fission/fetcher
 docker pull fission/fission-bundle
+docker pull fission/builder
 
 mkdir /root/hello.js
