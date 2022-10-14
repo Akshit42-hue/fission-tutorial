c4d86c5ad5ad8bdab4ed7625b089fc07759d6d3a
Akshit42-hue <patelakshit2025@gmail.com>
2022-10-14 21:33:14 +0530

    updated_3
    Signed-off-by: Akshit42-hue <patelakshit2025@gmail.com>

--- 2-lts/courseBase.sh
+++ 2-lts/courseBase.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-
+export FISSION_NAMESPACE="fission"
+kubectl create namespace $FISSION_NAMESPACE
+kubectl create -k "github.com/fission/fission/crds/v1?ref=v1.17.0"
+helm repo add fission-charts https://fission.github.io/fission-charts/
+helm repo update
+helm install --version v1.17.0 --namespace $FISSION_NAMESPACE fission \
+  --set serviceType=NodePort,routerServiceType=NodePort \
+  fission-charts/fission-all
+  
 docker pull fission/fission-bundle
 
 mkdir /root/hello.js
