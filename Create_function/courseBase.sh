9b0f35b374d91a108143c1bdc0138fe90e7578e6
Akshit42-hue <patelakshit2025@gmail.com>
2022-10-17 09:52:07 +0530

    updg
    Signed-off-by: Akshit42-hue <patelakshit2025@gmail.com>

--- Create_function/courseBase.sh
+++ Create_function/courseBase.sh
@@ -5,5 +5,14 @@ docker pull fission/fission-bundle
 docker pull fission/builder
 kubernetes-kubeadm-2nodes
 
+snap install helm --classic
 
+export FISSION_NAMESPACE="fission"
+kubectl create namespace $FISSION_NAMESPACE
+kubectl create -k "github.com/fission/fission/crds/v1?ref=v1.17.0"
+helm repo add fission-charts https://fission.github.io/fission-charts/
+helm repo update
+helm install --version v1.17.0 --namespace $FISSION_NAMESPACE fission \
+  --set serviceType=NodePort,routerServiceType=NodePort \
+  fission-charts/fission-all
 
